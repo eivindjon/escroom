2a1a10cc82723962c29169620fc69c57c3c3a872
eivindjon <eivind.b.jonassen@gmail.com>
2022-11-06 18:47:03 +0100

    jobbet med queries mot firebase

---
+++ src/components/Room.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+
+export default function Room({room}) {
+  return (
+    <div>
+      {room}
+    </div>
+  );
+}
--- src/pages/EscapeRooms.jsx
+++ src/pages/EscapeRooms.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import {db} from "../firebase-config";
+import { query, collection, onSnapshot } from "firebase/firestore";
+import Room from "../components/Room"
 
-function EscapeRooms() {
+export default function EscapeRooms() {
+  const [trails, setTrails] = useState(["Albebra 6.trinn", "Mongolske byer"]);
+
+
+
+  // READ ALL TRAILS FROM database
+  useEffect(() => {
+    const q = query(collection(db, "trails"))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let trailsArray = [];
+      querySnapshot.forEach((doc) => {
+        trailsArray.push({...doc.data(), id: doc.id})
+      });
+      setTrails(trailsArray)
+      console.log("resultat av query:", trailsArray)
+    })
+    return () => unsubscribe()
+  },[])
+
+  
+
   return (
-    <div>EscapeRooms</div>
-  )
+    <Container>
+      <Col>
+        <Row>
+          {trails.map((room, index) =>{
+            <Room key={index} room={room} />
+          })}
+        </Row>
+      </Col>
+    </Container>
+  );
 }
-
-export default EscapeRooms;
